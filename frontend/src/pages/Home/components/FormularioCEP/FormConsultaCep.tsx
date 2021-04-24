26c029f62116e8ac6bf067843efef08016a13ac1
João Pedro <joaopedrosf1903@gmail.com>
2021-04-24 10:00:13 -0300

    Integração página Home com API de CEPs

--- frontend/src/pages/Home/components/FormularioCEP/FormConsultaCep.tsx
+++ frontend/src/pages/Home/components/FormularioCEP/FormConsultaCep.tsx
@@ -1,27 +1,50 @@
 import React from 'react';
 import styles from './FormConsultaCep.module.css';
+import {requisicao} from '../../../../core/utils/requestUtils'
 
 export default function FormConsultaCep() {
     return(
         <div className={styles.container}>
             <form>
                 <div className={styles.formularioCep}>
-                    <input className={styles.inputCep} type="text" placeholder="Digite o CEP" pattern="[0-9]{8}"></input>
-                    <input className={styles.submitButton} type="submit" value="Consultar"></input>
+                    <input id="cep" 
+                    className={styles.inputCep} 
+                    type="text" 
+                    placeholder="Digite o CEP" 
+                    pattern="[0-9]{8}" />
+                    
+                    <input type="button" 
+                    value="Consultar" 
+                    className={styles.submitButton}
+                    onClick={() => {buscarCep()}} />
                 </div>
                 <div>
-                    <input type="text" disabled></input>
+                    <input id="logradouro" type="text" disabled></input>
                 </div>
                 <div>
-                    <input type="text" disabled></input>
+                    <input id="bairro" type="text" disabled></input>
                 </div>
                 <div>
-                    <input type="text" disabled></input>
+                    <input id="cidade" type="text" disabled></input>
                 </div>
                 <div>
-                    <input type="text" disabled></input>
+                    <input id="estado" type="text" disabled></input>
                 </div>
             </form>
         </div>
     );
+}
+
+function buscarCep() {
+    const cep: string = (document.getElementById("cep") as HTMLInputElement).value
+    const logradouroElement: HTMLInputElement = document.getElementById("logradouro") as HTMLInputElement
+    const bairroElement: HTMLInputElement = document.getElementById("bairro") as HTMLInputElement
+    const cidadeElement: HTMLInputElement = document.getElementById("cidade") as HTMLInputElement
+    const estadoElement: HTMLInputElement = document.getElementById("estado") as HTMLInputElement
+
+    requisicao({url: `https://viacep.com.br/ws/${cep}/json/`})
+    .then(response => {logradouroElement.value = response.data.logradouro;
+                        bairroElement.value = response.data.bairro
+                        cidadeElement.value = response.data.localidade
+                        estadoElement.value = response.data.uf});
 }
