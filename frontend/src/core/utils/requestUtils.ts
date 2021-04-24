5bb70001498ab471ef91ac293ed5fcbb402be85d
João Pedro <joaopedrosf1903@gmail.com>
2021-04-24 11:22:10 -0300

    Criação função de Login

--- frontend/src/core/utils/requestUtils.ts
+++ frontend/src/core/utils/requestUtils.ts
@@ -5,13 +5,36 @@ type RequestParams = {
     url: string;
     data?: object;
     params?: object;
+    headers?: object;
 }
 
-export const requisicao = ({method = 'GET', url, data, params}:RequestParams) => {
+type LoginData = {
+    username: string;
+    password: string;
+}
+
+const URL_BASE = "http://localhost:8080"
+
+export const requisicao = ({method = 'GET', url, data, params, headers}:RequestParams) => {
     return axios({
         method,
         url: `${url}`,
         data,
         params
     });
+}
+
+export const fazerLogin = (data:LoginData) => {
+    const payload = {
+        ...data,
+        grant_type: 'password'
+    }
+
+    const token = `grupo2:654321`
+    const headers = {
+        Authorization: `Basic ${window.btoa(token)}`,
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+
+    return requisicao({method: 'POST', url: `${URL_BASE}/oauth/token`, data: payload, headers})
 }
