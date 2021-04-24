fe7814d13c2c8837cdec930cb9beb4b9b69ed417
Lara Portilho <lara.portilho.m@gmail.com>
2021-04-24 18:52:14 -0300

    Configurado Login e Registrar para biblioteca react-hook-form

--- frontend/src/pages/Login/components/FormLogin/FormLogin.tsx
+++ frontend/src/pages/Login/components/FormLogin/FormLogin.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import styles from './FormLogin.module.css'
+import { useForm } from 'react-hook-form'
+import { fazerLogin } from '../../../../core/utils/requestUtils';
+
+type formstate = {
+    username: string;
+    password: string;
+}
+
+function onSubmit(data: formstate){
+    fazerLogin(data)
+    .then(response => {
+        console.log(response);
+    })
+}
 
 export default function FormLogin() {
+    const {register, handleSubmit, errors} = useForm<formstate>();
     return(
         <div className={styles.container}>
             <h2>Login</h2>
             <p>Entre na sua conta com seu email e senha.</p>
-            <form>
-                <input type="text" className={styles.input} placeholder="Email">
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input type="text" className={styles.input} 
+                    placeholder="Email" name="username" ref={register}>
                 </input><br /><br />
-                <input type="password" className={styles.input} placeholder="Senha">
+                <input type="password" className={styles.input} 
+                    placeholder="Senha" name="password" ref={register}>
                 </input><br /><br />
                 <input type="submit" className={styles.button} value="Login"></input><br /><br />
             </form>
--- frontend/src/pages/Registrar/components/FormRegistrar/FormRegistrar.tsx
+++ frontend/src/pages/Registrar/components/FormRegistrar/FormRegistrar.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
 import styles from './FormRegistrar.module.css'
+import { useForm } from 'react-hook-form'
+import { requisicao } from '../../../../core/utils/requestUtils';
+
+type formstate = {
+    nome: string;
+    email: string;
+    senha: string;
+}
+
+function onSubmit(data: formstate){
+    requisicao({method:'POST', url:'http://localhost:8080/registrar', data})
+    .then(() => {
+        console.log("Usuário cadastrado");
+    })
+}
 
 export default function FormRegistrar() {
+    const {register, handleSubmit, errors} = useForm<formstate>();
     return(
         <div className={styles.container}>
             <h2>Criar Conta</h2>
             <p className={styles.msg}>Informe seu nome, email e senha</p>
-            <form>
-                <input type="text" className={styles.input} placeholder="Nome">
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input type="text" className={styles.input}
+                    placeholder="Nome" name="nome" ref={register}>
                 </input><br /><br />
-                <input type="text" className={styles.input} placeholder="Email">
+                <input type="text" className={styles.input} 
+                    placeholder="Email" name="email" ref={register}>
                 </input><br /><br />
-                <input type="password" className={styles.input} placeholder="Senha*">
+                <input type="password" className={styles.input} 
+                placeholder="Senha*" name="senha" ref={register}>
                 </input><br /><br />
                 <p className={styles.obs}>*A senha deve ter pelo menos 6 caracteres</p>
                 <input type="submit" className={styles.button} value="Cadastrar"></input><br /><br />
