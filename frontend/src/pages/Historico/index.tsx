17c60c0e488513c4501f45a42d57e4d196f77b40
Podesta <11512008+Podesta@users.noreply.github.com>
2021-04-26 01:06:45 +0000

    Página do Histórico básica
    TODO:
     - Conectar com backend;
     - Função para filtrar resultados buscados;
     - Criar os cards de maneira dinâmica, e na quantidade correta;

--- frontend/src/pages/Historico/index.tsx
+++ frontend/src/pages/Historico/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import FormHistorico from './components/FormularioHistorico/FormHistorico';
+import './index.css'
+import {Link} from 'react-router-dom';
 
 export default function Historico() {
     return(
-        <h1>Historico</h1>
+        <div className="containerHist">
+            <h1>CONSULTA CEP</h1>
+            <h2>Processo Seletivo ELO Jr 2021 - Grupo 2</h2>
+            <Link to="/" className="buttonHist" >Consulta</Link>
+            <FormHistorico />
+        </div>
     );
 }
