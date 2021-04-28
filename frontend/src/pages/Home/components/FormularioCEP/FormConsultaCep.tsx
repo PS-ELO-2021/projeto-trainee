d3f3a96dc99d127a3103913e8d214f0bbeec6c95
João Pedro <joaopedrosf1903@gmail.com>
2021-04-28 08:39:42 -0300

    Mensagens de erro e validações login, registro e consulta de cep

--- frontend/src/pages/Home/components/FormularioCEP/FormConsultaCep.tsx
+++ frontend/src/pages/Home/components/FormularioCEP/FormConsultaCep.tsx
@@ -1,24 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './FormConsultaCep.module.css';
 import { requisicao } from '../../../../core/utils/requestUtils'
 import { getSessaoUsuarioAsLoginType } from '../../../../core/utils/auth';
 import { AxiosResponse } from 'axios';
 import { Busca } from '../../../../core/utils/types';
+import { useForm } from 'react-hook-form';
 
 export default function FormConsultaCep() {
+    const {register, handleSubmit, errors} = useForm();
+    const [isCepInvalido, setIsCepInvalido] = useState<boolean>(false)
+
+    const onSubmit = () => {
+        const cep: string = (document.getElementById("cep") as HTMLInputElement).value
+        const logradouroElement: HTMLInputElement = document.getElementById("logradouro") as HTMLInputElement
+        const bairroElement: HTMLInputElement = document.getElementById("bairro") as HTMLInputElement
+        const cidadeElement: HTMLInputElement = document.getElementById("cidade") as HTMLInputElement
+        const estadoElement: HTMLInputElement = document.getElementById("estado") as HTMLInputElement
+
+        setIsCepInvalido(false)
+
+        requisicao({url: `https://viacep.com.br/ws/${cep}/json/`})
+        .then(response => {
+            if(response.data.erro == true) {
+                throw 404;
+            }
+
+            logradouroElement.value = response.data.logradouro;
+            bairroElement.value = response.data.bairro
+            cidadeElement.value = response.data.localidade
+            estadoElement.value = response.data.uf
+
+            salvarCep(response)
+        })
+        .catch(() => {
+            setIsCepInvalido(true)
+        });
+    }
+
     return(
         <div className={styles.container}>
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <input id="cep" 
                 className={`${styles.inputCep} ${styles.inputHome}`} 
                 type="text" 
-                placeholder="Digite o CEP" 
-                pattern="[0-9]{8}" />
+                placeholder="Digite o CEP"
+                name="cep"
+                ref={register({
+                    pattern: {
+                        value: /[0-9]{8}/,
+                        message: "Cep deve conter apenas números!",
+                    },
+                    minLength: {
+                        value: 8,
+                        message: "Cep deve conter 8 números!"
+                    },
+                    maxLength: {
+                        value: 8,
+                        message: "Cep deve conter 8 números!"
+                    }
+                })}/>
+                
 
-                <input type="button" 
+                <input type="submit" 
                 value="Consultar" 
-                className={styles.submitButton}
-                onClick={() => {buscarCep()}} />
+                className={styles.submitButton} />
+                
+                {errors.cep && (
+                    <small>{errors.cep.message}</small>
+                )}
+                {isCepInvalido && (
+                    <small>Cep não encontrado</small>
+                )}
+
                 <input className={styles.inputHome} id="logradouro" type="text" disabled></input>
                 <input className={styles.inputHome} id="bairro" type="text" disabled></input>
                 <input className={styles.inputHome} id="cidade" type="text" disabled></input>
@@ -37,12 +90,19 @@ function buscarCep() {
 
     requisicao({url: `https://viacep.com.br/ws/${cep}/json/`})
     .then(response => {
+        if(response.data.erro == true) {
+            throw 400;
+        }
+
         logradouroElement.value = response.data.logradouro;
         bairroElement.value = response.data.bairro
         cidadeElement.value = response.data.localidade
         estadoElement.value = response.data.uf
 
         salvarCep(response)
+    })
+    .catch(() => {
+        
     });
 }
 
--- frontend/src/pages/Registrar/components/FormRegistrar/FormRegistrar.tsx
+++ frontend/src/pages/Registrar/components/FormRegistrar/FormRegistrar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './FormRegistrar.module.css'
 import { useForm } from 'react-hook-form'
 import { fazerLogin, requisicao } from '../../../../core/utils/requestUtils';
@@ -8,9 +8,12 @@ import { Link } from 'react-router-dom';
 
 export default function FormRegistrar() {
     const {register, handleSubmit, errors} = useForm<RegistrarFormState>()
+    const [isEmailJaCadastrado, setIsEmailJaCadastrado] = useState<boolean>(false)
     let history = useHistory()
 
     const onSubmit = (data: RegistrarFormState) => {
+        setIsEmailJaCadastrado(false)
+
         requisicao({method:'POST', url:'http://localhost:8080/registrar', data})
         .then(() => {
             const loginData: LoginData = {username: data.email, password: data.senha}
@@ -22,6 +25,9 @@ export default function FormRegistrar() {
                 history.push('/')
             })
         })
+        .catch(() => {
+            setIsEmailJaCadastrado(true)
+        })
     }
 
     return(
@@ -30,14 +36,41 @@ export default function FormRegistrar() {
             <p className={styles.msg}>Informe seu nome, email e senha</p>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <input type="text" className={styles.input}
-                    placeholder="Nome" name="nome" ref={register}>
+                    placeholder="Nome" name="nome" ref={register({
+                        required: "Campo obrigatório!"
+                    })}>
                 </input>
+                {errors.nome && (
+                    <small>{errors.nome.message}</small>
+                )}
                 <input type="text" className={styles.input} 
-                    placeholder="Email" name="email" ref={register}>
+                    placeholder="Email" name="email" ref={register({
+                        required: "Campo obrigatório!",
+                        pattern: {
+                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                            message: "Insira um email válido!",
+                        }
+                    })}>
                 </input>
+                {errors.email && (
+                    <small>{errors.email.message}</small>
+                )}
                 <input type="password" className={styles.input} 
-                placeholder="Senha*" name="senha" ref={register}>
+                placeholder="Senha*" name="senha" ref={register({
+                    required: "Campo obrigatório!",
+                    minLength: {
+                        value: 6,
+                        message: "A senha deve possuir no mínimo 6 caracteres!"
+                    }
+                })}>
                 </input>
+                {errors.senha && (
+                    <small>{errors.senha.message}</small>
+                )}
+                {isEmailJaCadastrado && (
+                    <small>Email já cadastrado!</small>
+                )}
+
                 <p className={styles.obs}>*A senha deve ter pelo menos 6 caracteres</p>
                 <input type="submit" className={styles.button} value="Cadastrar"></input>
             </form>
