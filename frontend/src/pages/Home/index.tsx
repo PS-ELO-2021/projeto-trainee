22e28b79e9c43ca46ac4692fb29bb8e08dad2602
Lara Portilho <lara.portilho.m@gmail.com>
2021-04-27 21:21:33 -0300

    Atualizar e padronizar o CSS

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -5,9 +5,9 @@ import {Link} from 'react-router-dom';
 
 export default function Home() {
     return(
-        <div className="container">
-            <h1 className="h1-home">CONSULTA CEP</h1>
-            <h2 className="h2-home">Processo Seletivo ELO Jr 2021 - Grupo 2</h2>
+        <div>
+            <h1 className="title">Consulta CEP</h1>
+            <h2 className="subtitle">Processo Seletivo ELO Jr 2021 - Grupo 2</h2>
 
 
             <Link to="/historico" className="buttonHist">
