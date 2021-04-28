fccca73acb9b7429626c44934e1567ed6c01f6a8
Podesta <11512008+Podesta@users.noreply.github.com>
2021-04-28 06:11:35 +0000

    Navbar teste 01

--- frontend/src/core/components/Navbar/Navbar.tsx
+++ frontend/src/core/components/Navbar/Navbar.tsx
@@ -1,38 +1,53 @@
 import styles from './Navbar.module.css'
-import {isAutenticado} from '../../utils/requestUtils'
+import { isAutenticado } from '../../utils/requestUtils'
 import { Redirect, useHistory } from 'react-router';
 import { getSessaoUsuarioAsLoginType } from '../../utils/auth';
 import { useState } from 'react';
 
 export default function Navbar() {
-    const [texto, setTexto] = useState<string>(
-        `${isAutenticado() ? `${getSessaoUsuarioAsLoginType().emailUsuario}` : "Faça login ou cadastre-se!"}`)
-        
+  const [texto, setTexto] = useState<string>(
+    `${isAutenticado() ? `${getSessaoUsuarioAsLoginType().emailUsuario}` : "Visitante"}`)
+
     let history = useHistory()
 
+    // Apagar? //
     function logout() {
+      localStorage.removeItem('token')
+      history.push('/login')
+    }
+
+    function navBut () {
+      if (isAutenticado()) {
         localStorage.removeItem('token')
         history.push('/login')
+      }
+      else {
+        history.push('/')
+      }
+
     }
 
     const mudarTexto = () => {
-        isAutenticado() ? setTexto(`${getSessaoUsuarioAsLoginType().emailUsuario}`) 
-        : setTexto("Faça login ou cadastre-se!");
+      isAutenticado() ? setTexto(`${getSessaoUsuarioAsLoginType().emailUsuario}`) 
+        : setTexto("Visitante");
     }
 
     history.listen(() => {
-        mudarTexto()
+      mudarTexto()
     })
 
     return(
-        <div className={styles.container}>
-            <button 
-            className={isAutenticado() ? styles.logoutButton : styles.displayNone} 
-            onClick={() => logout()}>
-                LOGOUT
-            </button>
-            <p className={styles.navbarText}>{texto}</p>
+      <div className={styles.container}>
+        <div className={styles.limit}>
+        <button 
+          className={isAutenticado() ? styles.logoutButton : styles.logoutButton} 
+          onClick={() => navBut()}>
+          {isAutenticado() ? "SAIR" : "HOME"}
+        </button>
         </div>
+        <p className={styles.navbarTitle}>Consulta CEP</p>
+        <p className={styles.navbarText}>{texto}</p>
+      </div>
     );
 }
 
--- frontend/src/pages/Historico/components/FormularioHistorico/FormHistorico.tsx
+++ frontend/src/pages/Historico/components/FormularioHistorico/FormHistorico.tsx
@@ -3,15 +3,16 @@ import { BuscaAPI } from '../../../../core/utils/types';
 import styles from './FormHistorico.module.css';
 
 export default function FormHistorico(props : BuscaAPI) {
-    return(
-        <div className={styles.container}>
-            <p className={styles.resultHist + ' ' + styles.resultHistCEP}>{props.cep}</p>
-            <p className={styles.resultHist}>{props.logradouro}</p>
-            <p className={styles.resultHist}>{props.bairro}</p>
-            <p className={styles.resultHist}>{props.cidade}</p>
-            <p className={styles.resultHist}>{props.estado}</p>
-        </div>
-    );
+  return(
+    <div className={styles.container}>
+      <p className={styles.resultHist + ' ' + styles.resultHistCEP}>{props.cep}</p>
+      <p className={styles.resultHist}>{props.logradouro}</p>
+      <p className={styles.resultHist}>{props.bairro}</p>
+      <p className={styles.resultHist}>{props.cidade}</p>
+      <p className={styles.resultHist}>{props.estado}</p>
+      <p className={styles.resultHist}>{props.criadoEm}</p>
+    </div>
+  );
 }
 
 
